8e390975983013291efd230da4dbc6569b302083
Lars Gimse <larsgimse@gmail.com>
2018-10-11 22:45:08 +0200

    Add files via upload

---
+++ webserver/wifi.py
@@ -0,0 +1,17 @@
+print("WiFi connection")
+ssid = input("SSID: ")
+password = input("Password: ")
+
+
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(str(ssid), str(password))
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
+do_connect()
